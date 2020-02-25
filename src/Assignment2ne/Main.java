bfa1a3266cca7d890611135f5c41684e7627dd38
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-02-25 22:35:06 +0700

    hí

--- src/Assignment2ne/Main.java
+++ src/Assignment2ne/Main.java
@@ -1,9 +1,17 @@
 package Assignment2ne;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         News news = new News();
-        news.Display();
+        news.setId(1);
+        news.setTitle("Măc kệ thiên hạ ");
+        news.setPublishDate("03/03/2017");
+        news.setAuthor("Mari Tamagawa");
+        news.setContent("Khi mùa đông lạnh lẽo vừa đi qua , mùa xuân ấm áp sẽ đến . Giây phút chấp nhận từ bỏ và gạt đi định kiến \ncủa người khác sống bằng chính kiến của bản thân sẽ là thời điểm bạn thấy tâm hồn mình thanh thản .");
         news.Calculate();
+        news.getAverageRate();
+        news.Display();
     }
 }
--- src/Assignment2ne/News.java
+++ src/Assignment2ne/News.java
@@ -1,6 +1,7 @@
 package Assignment2ne;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class News implements INews {
@@ -72,15 +73,13 @@ public class News implements INews {
 
     @Override
     public void Display() {
-        for (News news : news) {
-            System.out.println(news.getId());
-            System.out.println(news.getTitle());
-            System.out.println(news.getPublishDate());
-            System.out.println(news.getAuthor());
-            System.out.println(news.getContent());
-            System.out.println(news.getAverageRate());
+            System.out.println("- ID : "+getId());
+            System.out.println("- Title : "+getTitle());
+            System.out.println("- Publish Date : "+getPublishDate());
+            System.out.println("- Author : "+getAuthor());
+            System.out.println("- Content : "+getContent());
+            System.out.println("- Average Rate : "+getAverageRate());
 
-        }
     }
 
     int[] Ratelist = new int[3];
@@ -96,10 +95,19 @@ public class News implements INews {
     }
 
     public float Calculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập a :");
+        Ratelist[0] = scanner.nextInt();
+        System.out.println("nhập b :");
+        Ratelist[1] = scanner.nextInt();
+        System.out.println("nhập c :");
+        Ratelist[2] = scanner.nextInt();
         int sum = 0;
+        float averagerate=0;
         for (int i = 0; i < Ratelist.length; i++) {
             sum += Ratelist[i];
+            averagerate = sum/Ratelist.length;
         }
-        return sum / (Ratelist.length);
+        return AverageRate = averagerate;
     }
 }
