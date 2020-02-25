70ad03779e189a9bb1a33c50a401bd317c281dde
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-02-25 11:57:29 +0700

    push

---
+++ src/Assignment2/INews.java
@@ -0,0 +1,6 @@
+package Assignment2;
+
+
+public interface INews {
+    void Display();
+}
---
+++ src/Assignment2/Main.java
@@ -0,0 +1,9 @@
+package Assignment2;
+
+public class Main {
+    public static void main(String[] args) {
+        News news = new News();
+        news.Display();
+        news.Calculate();
+    }
+}
