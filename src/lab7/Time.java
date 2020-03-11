b85edbde825d8887678704c9dc503d6c55865e96
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-03-11 18:36:11 +0700

    sua chut lab7 nè

--- src/lab7/Time.java
+++ src/lab7/Time.java
@@ -6,11 +6,11 @@ public class Time extends Thread{
     public void run() {
         int minute = 10;
         int second = 59;
-        for (int i = minute ;i>0;i--){
-            for (int j = second ; j>0;j--) {
+        for (int i = minute ;i>=0;i--){
+            for (int j = second ; j>=0;j--) {
                 System.out.println(i+":"+j);
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(10);
                 } catch (Exception e) {
 
                 }
