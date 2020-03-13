7b2b98f235213d02fdd0a0a8f2923b496320e2c2
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-03-13 10:54:31 +0700

    bài thi java2

---
+++ src/examadf2/AddressBooks.java
@@ -0,0 +1,82 @@
+package examadf2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBooks{
+    ArrayList<Contact> list = new ArrayList<>();
+
+
+    public void addcontact(Contact contact){
+        list.add(contact);
+    }
+    public void displaycontact(){
+        System.out.println("thông tin :");
+        for(Contact c : list){
+            System.out.println(c.toString());
+        };
+    }
+
+    public boolean findContact(String contactName){
+        for(int i=0;i<list.size();i++){
+            Contact contact = list.get(i);
+            if(contact.getName().equals(contactName)){
+                System.out.println("Name: "+contact.getName()+"Company:"+contact.getCompany()+"Email: "+contact.getName()+"Phone:"+contact.getPhone());
+                return true;
+            }
+        }
+        System.out.println("Không tìm thấy liên lạc !");
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        AddressBooks a = new AddressBooks();
+        int pess;
+        String name;
+        String phone;
+        String company;
+        String email;
+        do{
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Bắt Đầu Chương Trình");
+            System.out.println("1. Nhập Thông tin ");
+            System.out.println("2. Tìm kiếm liên lạc");
+            System.out.println("3. Hiển thị liên lạc ");
+            System.out.println("4. kết thúc .");
+            System.out.println("--------------------");
+            System.out.println("nhập số :");
+            System.out.println("");
+            pess = sc.nextInt();
+            switch (pess) {
+                case 1:
+                    System.out.println("Nhập tên của bạn :");
+                    name = sc.next();
+                    System.out.println("Nhập tên công ty :");
+                    company = sc.next();
+                    System.out.println("Nhập địa chỉ Email :");
+                    email = sc.next();
+                    System.out.println("Nhập số điện thoại :");
+                    phone = sc.next();
+                    a.addcontact(new Contact(name,company,email,phone));
+                    break;
+                case 2:
+                    String nameFind;
+                    System.out.println("Tìm kiếm tên liên lạc : ");
+                    nameFind = sc.next();
+                    a.findContact(nameFind);
+                    break;
+                case 3:
+                    a.displaycontact();
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Không có nè vui lòng chọn lại nhé , yêu thương <3");
+                    System.out.println("----- ----- ----- -----\n\n");
+            }
+        }while(pess!=0);
+    }
+}
---
+++ src/examadf2/Contact.java
@@ -0,0 +1,53 @@
+package examadf2;
+
+public class Contact {
+    String name;
+    String company;
+    String email;
+    String phone;
+
+
+    public Contact(String name, String company, String email, String phone) {
+        this.name = name;
+        this.company = company;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "name='" + name + '\'' + ", company='" + company + '\'' + ", email='" + email + '\'' + ", phone='" + phone + '\'' + '}';
+    }
+}
